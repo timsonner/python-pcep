fb69722f63aaa91f3ddf4bb659a14e0a83c0c65f
opc <opc@example.com>
2026-03-15 18:13:39 +0000

    add number check for 1337ness

--- block1/1_3_practice.py
+++ block1/1_3_practice.py
@@ -7,12 +7,30 @@ hex_leet_num = 0x539
 int_leet_num = 1337 
 float_leet_num = 1337e3
 squared_leet_num = int_leet_num ** 2
+is_leet = False
 
-print("type cast formatting")
-print("Integer:", int_leet_num,"Float:", float_leet_num, "Binary:", bin(int_leet_num),"Octal:", oct(int_leet_num), "Hexadecimal:", hex(int_leet_num))
-print()
-print("f-string formatting")
+print("f-string print formatting")
 print(f"Integer: {int_leet_num}, Float: {float_leet_num:.3f}, Binary: {int_leet_num:016b}, Octal: {int_leet_num:08o}, Hexadecimal: {int_leet_num:08x}")
 print()
 print("Squared:", squared_leet_num)
 print(f"Square root: {math.sqrt(int_leet_num):.3f}")
+print()
+print("type cast print formatting")
+print("Integer:", int_leet_num,"Float:", float_leet_num, "Binary:", bin(int_leet_num),"Octal:", oct(int_leet_num), "Hexadecimal:", hex(int_leet_num))
+
+user_input = input("Check a number: ")
+
+if user_input == str(int_leet_num):
+    is_leet = True
+elif user_input == bin(bin_leet_num):
+    is_leet =True
+elif user_input == oct(oct_leet_num):
+    is_leet =True
+elif user_input == hex(hex_leet_num):
+    is_leet =True
+elif user_input == str(float(float_leet_num)):
+    is_leet =True
+else:
+    print("Sorry not in my list, try again.")
+
+print("Status of is_leet:", is_leet)
